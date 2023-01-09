20a043ab92a67af56bbf1a238ca129f9aa6f6f55
DroxKiwi <corentinfredj.pro@gmail.com>
2023-01-09 04:01:57 +0100

    ajout page js.html et resizable.js avec le css nécessaire

---
+++ src/js/resizable.js
@@ -0,0 +1,43 @@
+/*Make resizable div by Hung Nguyen*/
+/* modified for the Studi evaluation */
+function makeResizableDiv(div) {
+    const element = document.querySelector(div);
+    const resizers = document.querySelectorAll(div + ' .resizer')
+    const minimum_size = 100
+    let original_width = 0;
+    let original_height = 0;
+    let original_x = 0;
+    let original_y = 0;
+    let original_mouse_x = 0;
+    let original_mouse_y = 0;
+    for (let i = 0;i < resizers.length; i++) {
+      const currentResizer = resizers[i];
+      currentResizer.addEventListener('mousedown', function(e) {
+        e.preventDefault()
+        original_width = parseFloat(getComputedStyle(element, null).getPropertyValue('width').replace('px', ''));
+        original_height = parseFloat(getComputedStyle(element, null).getPropertyValue('height').replace('px', ''));
+        original_x = element.getBoundingClientRect().left;
+        original_y = element.getBoundingClientRect().top;
+        original_mouse_x = e.pageX;
+        original_mouse_y = e.pageY;
+        window.addEventListener('mousemove', resize)
+        window.addEventListener('mouseup', stopResize)
+      })
+      
+      function resize(e) {
+        if (currentResizer.classList.contains('top-right')) {
+          const width = original_width + (e.pageX - original_mouse_x)
+          console.log(width)
+          if (width > minimum_size) {
+            element.style.width = width + 'px'
+          }
+        }
+      }
+      
+      function stopResize() {
+        window.removeEventListener('mousemove', resize)
+      }
+    }
+  }
+  
+  makeResizableDiv('.resizable')
